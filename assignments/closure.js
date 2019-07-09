11bd43d3ab074747032d83ecb46db281fb93a47f
Trevor Thompson <trevthom18@gmail.com>
2019-07-08 23:13:15 -0400

    Working on Closure stretch now

--- assignments/closure.js
+++ assignments/closure.js
@@ -32,12 +32,19 @@ grandfather(lastName);
 
 
 // ==== Challenge 2: Create a counter function ====
+
+let countTotal = 0;
+
 const counter = () => {
+
+   countTotal++;
+   return countTotal;
+
   // Return a function that when invoked increments and returns a counter variable.
 };
-// Example usage: const newCounter = counter();
-// newCounter(); // 1
-// newCounter(); // 2
+const newCounter = counter();
+ console.log(newCounter); // 1
+ console.log(newCounter); // 2
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
