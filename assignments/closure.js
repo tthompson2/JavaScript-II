7bdfed5bb3ae5b374b676586e5ae5858360d1b84
Trevor Thompson <trevthom18@gmail.com>
2019-07-15 22:47:10 -0400

    Counter Strecth Function now properly increments, but doesn't output the correct values that are specified by the problem

--- assignments/closure.js
+++ assignments/closure.js
@@ -33,18 +33,25 @@ grandfather(lastName);
 
 // ==== Challenge 2: Create a counter function ====
 
-let countTotal = 0;
+
 
 function counter () {
 
-   countTotal++;
-   return countTotal;
+   let countTotal = 0;
+
+   function counterIncrementer (){
+
+    return countTotal++;
+   }
+
+   return counterIncrementer;
 
   // Return a function that when invoked increments and returns a counter variable.
 };
  const newCounter = counter();
- console.log(newCounter); // 1
- console.log(newCounter); // 2
+ console.log(newCounter()); // 1
+ console.log(newCounter()); // 2
+ console.log(newCounter());
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
