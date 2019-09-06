1d5fabdfc926613b7a9c906b52bc7e20a136e1d6
Trevor Thompson <trevthom18@gmail.com>
2019-09-06 10:21:08 -0400

    Attempt stretch assignment

--- assignments/closure.js
+++ assignments/closure.js
@@ -43,8 +43,10 @@ function counter () {
 
     return countTotal++;
    }
-
+   countTotal++;
    return counterIncrementer;
+
+   // I honestly don't understand how this works
 
   // Return a function that when invoked increments and returns a counter variable.
 };
@@ -55,6 +57,25 @@ function counter () {
 
 // ==== Challenge 3: Create a counter function with an object that can increment and decrement ====
 const counterFactory = () => {
+
+  const value = {
+
+    number: 0,
+    
+    increment: function() {
+      number++;
+    },
+    decrement: function() {
+      number--;
+    },
+
+  };
+
+  function increment() {
+     value++;
+  }
+
+  function decrement
   // Return an object that has two methods called `increment` and `decrement`.
   // `increment` should increment a counter variable in closure scope and return it.
   // `decrement` should decrement the counter variable and return it.
